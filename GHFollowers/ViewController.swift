4100c4511c497d6d0ecf3c7212ba56000b557f95
Elchin Ismailov <ismailowwork@gmail.com>
2025-01-30 01:05:06 +0400

    Initial commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Elchin Ismailov on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
