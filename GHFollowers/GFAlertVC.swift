e3b7623b3fe4766a7918f2ae447d8b3ba405be91
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-01 23:59:02 +0400

    Create GFTitleLabel, GFBodyLabel and GFAlertVC

---
+++ GHFollowers/GFAlertVC.swift
@@ -0,0 +1,47 @@
+//
+//  GFAlertVC.swift
+//  GHFollowers
+//
+//  Created by Elchin Ismailov on 1.02.2025.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+    
+    let containerView = UIView()
+    let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel = GFBodyLabel(textAlignment: .center)
+    let actionButton = GFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle = title
+        self.message = message
+        self.buttonTitle = buttonTitle
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+    }
+    
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor = .systemBackground
+        containerView.layer.cornerRadius = 16
+        containerView.layer.borderWidth = 2
+        containerView.layer.borderColor = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
