0c4a9f35b06085d604eafcb55b34b885482cab15
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-02 20:15:50 +0400

    Add GFAlertVC to UIViewController Extension

---
+++ GHFollowers/UIViewController+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController+Ext.swift
+//  GHFollowers
+//
+//  Created by Elchin Ismailov on 2.02.2025.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func presentGFAlertOnMainThread(title: String, message: String, buttonTitle: String) {
+        DispatchQueue.main.async {
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
+            self.present(alertVC, animated: true)
+        }
+    }
+}
