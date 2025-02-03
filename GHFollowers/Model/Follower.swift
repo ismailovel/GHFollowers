b70c8ed8553771850b2b9b8078f90e104bbcb732
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-03 16:02:29 +0400

    Create Follower and User models

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Ismailov on 3.02.2025.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
