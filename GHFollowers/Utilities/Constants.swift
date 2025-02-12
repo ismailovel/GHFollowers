72caf4e9078696f28ec756ac70faa6909e38021a
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-12 13:49:33 +0400

    feat: create and add childVC to UserInfoVC

---
+++ GHFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GHFollowers
+//
+//  Created by Elchin Ismailov on 11.02.2025.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
