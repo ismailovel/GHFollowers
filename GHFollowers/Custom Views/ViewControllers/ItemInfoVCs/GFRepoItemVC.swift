5afc3b01a756b30194e0949060ec21fb2860165a
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-13 19:05:07 +0400

    feat: create and add subclasses GFRepoItemVC and GFFollowerItemVC to parent ViewController UserInfoVC

---
+++ GHFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemVC.swift
+//  GHFollowers
+//
+//  Created by Elchin Ismailov on 13.02.2025.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}
