61e781b06f7787548a58d219a9795d705bb4fea8
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-16 23:59:06 +0400

    feat: create PersistenceManager

---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,17 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by Elchin Ismailov on 16.02.2025.
+//
+
+import Foundation
+
+enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
+    
+    static func retrieveFavorites(completion: @escaping (Result<[Follower], GFError>) -> Void) {
+        
+    }
+}
