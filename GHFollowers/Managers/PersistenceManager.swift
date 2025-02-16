b9e6d086b91c79c14e79949831d6235c13514791
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-17 00:21:52 +0400

    feat: add retrieveFavorites from UserDefaults

--- GHFollowers/Managers/PersistenceManager.swift
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -9,9 +9,24 @@ import Foundation
 
 enum PersistenceManager {
     
+    enum Keys {
+        static let favorites = "favorites"
+    }
+    
     static private let defaults = UserDefaults.standard
     
     static func retrieveFavorites(completion: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completion(.success([]))
+            return
+        }
         
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completion(.success(favorites))
+        } catch {
+            completion(.failure(.unableToFavorite))
+        }
     }
 }
