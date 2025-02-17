0ed4908459310e86eb7d20c27d181a18a72c1849
Elchin Ismailov <ismailowwork@gmail.com>
2025-02-17 15:49:59 +0400

    feat: add and retrieve favorites

--- GHFollowers/Managers/PersistenceManager.swift
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -7,13 +7,43 @@
 
 import Foundation
 
+enum PersistenceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
+    
+    static private let defaults = UserDefaults.standard
     
     enum Keys {
         static let favorites = "favorites"
     }
     
-    static private let defaults = UserDefaults.standard
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completion: @escaping (GFError?) -> Void) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else {
+                        completion(.alreadyInFavorites)
+                        return
+                    }
+                    
+                    retrievedFavorites.append(favorite)
+                case .remove:
+                    retrievedFavorites.removeAll { $0.login == favorite.login }
+                }
+                
+                completion(save(favorites: retrievedFavorites))
+                
+            case .failure(let error):
+                completion(error)
+            }
+        }
+    }
     
     static func retrieveFavorites(completion: @escaping (Result<[Follower], GFError>) -> Void) {
         guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
@@ -27,6 +57,17 @@ enum PersistenceManager {
             completion(.success(favorites))
         } catch {
             completion(.failure(.unableToFavorite))
+        }
+    }
+    
+    static func save(favorites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let favoritesData = try encoder.encode(favorites)
+            defaults.set(favoritesData, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
         }
     }
 }
